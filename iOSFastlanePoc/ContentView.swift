259421aa8a186824563cd216f3dfe1ec370f0f79
Mohamed Afsar <smartafsarmbbs@gmail.com>
2023-11-10 10:21:16 -0800

    chore: Setup project

---
+++ iOSFastlanePoc/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iOSFastlanePoc
+//
+//  Created by Mohamed Afsar Shakeer on 2023-11-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOSFastlanePoc/iOSFastlanePocApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSFastlanePocApp.swift
+//  iOSFastlanePoc
+//
+//  Created by Mohamed Afsar Shakeer on 2023-11-10.
+//
+
+import SwiftUI
+
+@main
+struct iOSFastlanePocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
